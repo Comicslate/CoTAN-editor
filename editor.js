b202401633ce834dc5379ea407665da0a72c6745
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-02-02 01:08:56 +1000

    Update editor.js

--- editor.js
+++ editor.js
@@ -1,5 +1,5 @@
 // ВВОДНЫЕ
-console.log ( 'CoTAN ver. 2020.12.07 05:24 GMT+10' );
+console.log ( 'CoTAN ver. 2021.02.02 01:07 GMT+10' );
 var lang = JSINFO [ 'lang' ],
 	ct_text = [ ],
 	ct_texts = {
@@ -1164,7 +1164,7 @@ function renderText ( text ) { // обработка шрифтотегов
 	//стили реплик отдельных персонажей
 	//# freefall
 	result = result
-	.replace ( /\[(flo|sam|hlx|saw|qwe|dvo|edge|blunt|max|rai|kor|mad|mayor|mhlp|pol|mst1?|bow|ish|oth)\]/g, '<span class = "fest $1" style = "font-size: 1.3em">' )
+	.replace ( /\[(flo|sam|hlx|saw|qwe|dvo|edge|blunt|max|rai|kor|mad|mayor|mhlp|pol|mst1?|bow|ish|gre|oth)\]/g, '<span class = "fest $1" style = "font-size: 1.3em">' )
 	.replace ( /\[nio\]/g, '<span class = "fest niomi" style = "font-size: 1.3em">' )
 	.replace ( /\[com\]/g, '<span class = "fest edge" style = "font-size: 1.3em">' );
 	//# kitty
